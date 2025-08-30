5c2b83694b8e3e25e02a60d5fe3bb49de5a0a859
Alperen Türkseven <20010310029@ogrenci.bartin.edu.tr>
2025-08-30 06:06:17 +0300

    CORS ayarı güncellendiiii

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,14 +15,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   credentials: true,
 }));
 app.use(helmet());
